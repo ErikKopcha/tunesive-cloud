ad9074dee453df4c83e530d2de24486ec1aa2fdb
EK <erickopcha@gmail.com>
2022-09-30 16:36:51 +0300

    feat: shazam core API

---
+++ src/store/services/shazamCore.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1/",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", "");
+      // headers.set("X-RapidAPI-Host", "shazam-core.p.rapidapi.com")
+  
+      return headers;
+    }
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({
+      query: () => `/charts/world`
+    })
+  })
+})
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
