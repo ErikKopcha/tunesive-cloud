6f7b0733c94aa1c658cca671fea3a0048f288c7e
EK <erickopcha@gmail.com>
2022-10-01 16:39:22 +0300

    update: shazam core

--- src/store/services/shazamCore.js
+++ src/store/services/shazamCore.js
@@ -5,7 +5,7 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://shazam-core.p.rapidapi.com/v1/",
     prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-Key", import.meta.env.RAPID_API_KEY);
+      headers.set("X-RapidAPI-Key", import.meta.env.VITE_RAPID_API_KEY);
       // headers.set("X-RapidAPI-Host", "shazam-core.p.rapidapi.com")
   
       return headers;
